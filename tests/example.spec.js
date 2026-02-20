0a3e5f79cfe2ec90dfd0bcf180526e5518308af0
MohanaPriyaThiru <mohanapriyat1190@gmail.com>
2026-02-20 20:40:05 +0530

    Update example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -10,7 +10,7 @@ test('has title', async ({ page }) => {
 
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-  console.log("code changed") //new line added to this commit save done gvbvb
+  console.log("code changed") //new line added to this commit save done gvbvb file
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
