76cdebe5b61a48205e8689a7787f35d521eddaa2
MohanaPriyaThiru <mohanapriyat1190@gmail.com>
2026-02-20 20:35:25 +0530

    Update example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -5,7 +5,7 @@ test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/)//assertion;
+  await expect(page).toHaveTitle(/Playwright/)//assertion done;
 });
 
 test('get started link', async ({ page }) => {
