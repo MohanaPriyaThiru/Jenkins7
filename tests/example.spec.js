0602947740026371356b1e3eba8e943f34ad0d7b
MohanaPriyaThiru <mohanapriyat1190@gmail.com>
2026-02-20 20:05:26 +0530

    Update example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -10,7 +10,7 @@ test('has title', async ({ page }) => {
 
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-  console.log("code changed")
+  console.log("code changed") //new line added
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
